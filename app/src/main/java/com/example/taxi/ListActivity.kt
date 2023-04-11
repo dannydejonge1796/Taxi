f4958e7b1a7b73fd4385b43837a6cecf2421c79a
Danny de Jonge <dannydejonge1796@gmail.com>
2023-04-11 14:03:02 +0200

    tekst wordt weergegeven op andere pagina, en wat styling aangepast

--- app/src/main/java/com/example/taxi/ListActivity.kt
+++ app/src/main/java/com/example/taxi/ListActivity.kt
@@ -2,10 +2,33 @@ package com.example.taxi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.widget.Toolbar
 
 class ListActivity : AppCompatActivity() {
-  override fun onCreate(savedInstanceState: Bundle?) {
+
+  override fun onCreate(savedInstanceState: Bundle?)
+  {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_list)
+
+    //Toolbar ophalen en als default toolbar instellen
+    val toolbar = findViewById<Toolbar>(R.id.toolbar)
+    setSupportActionBar(toolbar)
+
+    //Tekst weergeven
+    this.showText()
+  }
+
+  private fun showText()
+  {
+    //Tekstveld ophalen d.m.v.
+    val textView = findViewById<TextView>(R.id.textView)
+    //Meegegeven data ophalen
+    val inputText = intent.getStringExtra("input_text")
+    //Bericht klaarmaken
+    val message = getString(R.string.input_text_message, inputText)
+    //Bericht als tekst instellen
+    textView.text = message
   }
 }
