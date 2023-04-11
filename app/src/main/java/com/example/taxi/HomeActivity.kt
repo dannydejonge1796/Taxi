fd2647ca2d896d762477c9b1d360623f1a9b0877
Danny de Jonge <dannydejonge1796@gmail.com>
2023-04-11 14:17:43 +0200

    home.xml hernoemd naar activity_home

--- app/src/main/java/com/example/taxi/HomeActivity.kt
+++ app/src/main/java/com/example/taxi/HomeActivity.kt
@@ -12,7 +12,7 @@ class HomeActivity : AppCompatActivity() {
   override fun onCreate(savedInstanceState: Bundle?)
   {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.home)
+    setContentView(R.layout.activity_home)
 
     //Toolbar ophalen en als default toolbar instellen
     val toolbar = findViewById<Toolbar>(R.id.toolbar)
